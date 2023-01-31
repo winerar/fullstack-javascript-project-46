107eb3c955b49fbbba8ac9e443f9f203a97d1c41
Alexander Karachevtsev <48007528+winerar@users.noreply.github.com>
2023-01-31 18:10:29 +0400

    Add read JSON

--- gendiff.js
+++ gendiff.js
@@ -1,9 +1,26 @@
 const { program } = require('commander');
+const fs = require('fs');
+const path = require('path');
+
+const readFile = (filepath) => {
+  const text = fs.readFileSync(filepath,'utf8')
+  return text;
+};
+
+const getJsonFromFile = (filepath) => {
+  return JSON.parse(readFile(filepath));
+};
+
+const gendiff = async (filepath1, filepath2) => {
+  console.log(getJsonFromFile(filepath1));
+  console.log(getJsonFromFile(filepath2));
+};
 
 program
   .description('Compares two configuration files and shows a difference.')
   .argument('<filepath1>')
   .argument('filepath2')
+  .action((filepath1, filepath2) => gendiff(filepath1, filepath2))
   .version('0.0.1', '-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format');
 
